beace9dc4de7991a501f44093221ff7ab792481a
Marx-code <karlmarxjesusmoraesdev@gmail.com>
2025-11-02 09:08:39 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Nav from './components/nav.tsx'
+
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='d-flex '>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import logo from '../assets/logo.jpg';
+
+
+
+function Nav() {
+    return (
+      <nav className="navbar bg-body-tertiary fixed-top bg-dark-subtle
+">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src={logo} width="30" height="24" className="d-inline-block align-text-top"/>
+      Bootstrap
+    </a>
+  </div>
+</nav>
+    );
+}
+
+export default Nav;
